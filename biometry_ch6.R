e2e3875a25c5c1fb4d3e4b7b9fee30d604fe6ff3
baeolophus <curryclairem@gmail.com>
2016-11-28 11:23:23 -0600

    Outlining what code needs written for chapter 6.

---
+++ biometry_ch6.R
@@ -0,0 +1,10 @@
+#Chapter 6 moves on from discrete distributions to the normal distribution, 
+#a continuous distribution that will be the basis of most statistics in the book.
+
+#Following box 6.2 to manually make a Q-Q plot to understand how they are built.
+
+#Then how to do them in R automatically.
+
+#The rankits in box 6.2 are done this way.
+
+#Look up Q-Q calculation in R to see how it deals with small sample sizes.

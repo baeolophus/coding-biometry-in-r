f3e13d02b5e770179e47b12feb252a5256a61893
baeolophus <curryclairem@gmail.com>
2016-11-29 17:56:47 -0600

    basic normal probability density function, equation 6.1, but haven't filled in pi yet and need to doublecheck.

--- biometry_ch6.R
+++ biometry_ch6.R
@@ -1,6 +1,15 @@
 #Chapter 6 moves on from discrete distributions to the normal distribution, 
 #a continuous distribution that will be the basis of most statistics in the book.
 
+#Let's check where it is.
+?Distributions
+?dnorm
+
+#Section 6.2 Properties of the Norman Distribution
+#We can draw a function as a plot using curve().
+
+curve((1/(1*sqrt(2*3.14159))*exp(-0.5*((x-1)/1)^2)), 0, 1, n = 101, add = FALSE, type = "l")
+
 #Following box 6.2 to manually make a Q-Q plot to understand how they are built.
 
 #Then how to do them in R automatically.

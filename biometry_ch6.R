06ac49c0c1bf9ceb64b91d5a090d15fb6462113a
baeolophus <curryclairem@gmail.com>
2016-11-30 09:34:02 -0600

    Edited normal probability curve to include actual pi, changed range which gave the full curve, and added R's dnorm easy version to get it, comparing by overlaying in plot.

--- biometry_ch6.R
+++ biometry_ch6.R
@@ -7,9 +7,23 @@
 
 #Section 6.2 Properties of the Norman Distribution
 #We can draw a function as a plot using curve().
+normal.manual<-function(mean, sd){
+curve((1/(sd*sqrt(2*pi)))*exp((-((x-mean)/sd)^2)/2),
+      -4, 4, #go from -4 to +4 standard deviations.
+      add = FALSE,
+      ylab="freq",
+      xlab="Y",
+      type = "l")
+}
+normal.manual(0,1)
 
-curve((1/(1*sqrt(2*3.14159))*exp(-0.5*((x-1)/1)^2)), 0, 1, n = 101, add = FALSE, type = "l")
-
+#You can get this automatically in R with dnorm.
+curve(dnorm(x, mean=0, sd=1),
+      -4, 4,
+      col="white",
+      lty="dashed",
+      add=TRUE)
+#I added a white dashed line over the original black line so you can see that they follow the same path.
 #Following box 6.2 to manually make a Q-Q plot to understand how they are built.
 
 #Then how to do them in R automatically.

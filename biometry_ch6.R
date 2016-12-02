358be9558c42c2d91d805941cdae4547b35f744b
baeolophus <curryclairem@gmail.com>
2016-12-02 16:35:05 -0600

    Code for getting various quantiles, probability density, cumulative curves, along with explanations and links.

--- biometry_ch6.R
+++ biometry_ch6.R
@@ -1,12 +1,16 @@
 #Chapter 6 moves on from discrete distributions to the normal distribution, 
 #a continuous distribution that will be the basis of most statistics in the book.
 
+#Section 6.1 discusses frequency distributions of continuous variables briefly.
+
+#Section 6.2 Properties of the Normal Distribution
+
 #Let's check where it is.
 ?Distributions
 ?dnorm
 
-#Section 6.2 Properties of the Norman Distribution
-#We can draw a function as a plot using curve().
+#We can draw the normal distribution in a custom function as a plot using curve()
+#and equation 6.1 on page 95.
 normal.manual<-function(mean, sd){
 curve((1/(sd*sqrt(2*pi)))*exp((-((x-mean)/sd)^2)/2),
       -4, 4, #go from -4 to +4 standard deviations.
@@ -24,6 +28,70 @@ curve(dnorm(x, mean=0, sd=1),
       lty="dashed",
       add=TRUE)
 #I added a white dashed line over the original black line so you can see that they follow the same path.
+
+#You can draw any of the distributions in Figure 6.2 using these functions 
+#(either the R one or the one we just made).
+
+#Figure 6.3 shows the cumulative normal distribution function.
+
+#Let's do that and then plot the normal probability density function on it.
+#Cumulative first since that plot will go from 0 to 1.
+curve(pnorm(x, mean=0, sd=1),
+      -4, 4,
+      lty="solid")
+
+#Then add the normal probability density function (like the one we made earlier, just with the default line color).
+curve(dnorm(x, mean=0, sd=1),
+      -4, 4,
+      lty="dashed",
+      add=TRUE)
+
+#To get out some of the values  shown in page 96, we use pnorm().
+#http://stackoverflow.com/questions/34236648/r-function-to-calculate-area-under-the-normal-curve-between-adjacent-standard-de
+
+pnorm(0, mean=0, sd=1)
+#Just adding one value gives you a point estimate.
+#50% will be found at zero.
+
+#The other values shown in Figure 6.3
+pnorm(-1)
+pnorm(-2)
+#0 and 1 are the default values, so you can leave them out if you want in this case.
+#If you add more than one, it will tell you the values.
+(pnorm(0:1))
+#Use diff as suggested here to get the area under the curve.
+diff(pnorm(0:1))
+#This works because it uses the cumulative function to calculate it.
+#If you go from -1 to 1 you get the 68.27% shown under Figure 6.2 and on page 96.
+
+#To go the other way, i.e. to see where 50% of the items fall, you have to use qnorm.
+#Let's see what qnorm looks like.
+curve(qnorm(x))
+#It is essentially tipping Figure 6.3 on its side.
+#The frequency is now x on the x-axis, and the standard deviations are the y axis.
+#You can get the point values in Figure 6.3 this way:
+qnorm(0.5)
+qnorm(0.1587)
+qnorm(0.0228)
+#The last two are close with rounding since those values in Figure 6.3 were rounded.
+#How to get the values on page 96, of where 50%, 95%, etc of items are found?
+qnorm(0.5+0.5*0.5)
+qnorm(0.95+0.5*0.05)
+qnorm(0.99+0.5*0.01)
+#The values that you input are not the percent values given on page 96.
+#You put them in proportions (the quantile you want to get) PLUS
+#half of the remaining value.
+#If you put in 0.95, it will go from 0 to 0.95.  That isn't centered at the mean of 0,
+#where the 50% quantile is.  There is 0.05 left at the end.  So we divide it by two
+#to put it on either end.
+#You would get the negative number for the standard deviation if you put in the opposite value.
+qnorm(0.5*0.01)
+#This also explains it nicely down where it starts talking about qnorm.
+#https://cran.r-project.org/web/packages/tigerstats/vignettes/qnorm.html
+
+qnorm(.99,
+      lower.tail=FALSE)
+#Section 6.3
 #Following box 6.2 to manually make a Q-Q plot to understand how they are built.
 
 #Then how to do them in R automatically.

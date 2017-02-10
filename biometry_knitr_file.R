f2ea68c621ef08d5b7a9872645e8122cb65a2770
baeolophus <curryclairem@gmail.com>
2017-02-10 14:01:25 -0600

    Deleted separate knitr file because I can specify html_fragment only in the output type in main Rmd file. Worked on wording in section 6.7.  Changing ranking test numbers to see how it works.  Comparing to order(order()) which is used in R's qqplot.

--- biometry_knitr_file.R
+++
@@ -1,7 +0,0 @@
-library(knitr)
-library(markdown)
-
-knit("biometry_ch6.Rmd") # produces a .md file
-markdownToHTML("biometry_ch6.md", "biometry_ch6.html", 
-               fragment.only=TRUE)
-#from http://rstudio-pubs-static.s3.amazonaws.com/30704_583c8c8e2b254bf6a0791543d4a3438b.html
